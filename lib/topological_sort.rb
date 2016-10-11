7c7d9e3ea1172b74f003b637c16f752ee27ecf46
Josh Henke <henke.josh@gmail.com>
2016-10-10 20:33:28 -0700

    began Khan topological sort

--- lib/topological_sort.rb
+++ lib/topological_sort.rb
@@ -2,6 +2,23 @@ require_relative 'graph'
 
 # Implementing topological sort using both Khan's and Tarian's algorithms
 
+# Khan
 def topological_sort(vertices)
+  sorted_list = []
+  count_dict = Hash.new(0)
 
+  vertices.each do |vtex|
+    count_dict[vtext] = vtex.in_edges.length
+  end
+
+  queue = count_dict.select { |k, v| v == 0 }.keys
+
+  until queue.empty?
+    u = queue.shift
+    sorted_list << u
+    vertices.delete(u)
+
+    
+
+  end
 end
